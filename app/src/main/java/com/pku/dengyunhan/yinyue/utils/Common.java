ef633c0dfae824ee8a0e8eed52ed05b7d33e0997
dengyunhan <18811021879@163.com>
2019-01-20 15:21:09 +0800

    dyh

---
+++ app/src/main/java/com/pku/dengyunhan/yinyue/utils/Common.java
@@ -0,0 +1,15 @@
+package com.pku.dengyunhan.yinyue.utils;
+
+import com.pku.dengyunhan.yinyue.entity.Music;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by shaoyangyang on 2017/12/20.
+ */
+
+public class Common {
+
+    public static List<Music> musicList = new ArrayList<>();
+}
